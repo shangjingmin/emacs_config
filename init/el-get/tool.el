0b2ee0acad58322ab275ad7df8aaac0c60c50c68
sjm <shangjingmin@gmail.com>
2013-07-17 13:58:52 +0800

    w3m

--- init/el-get/tool.el
+++ init/el-get/tool.el
@@ -48,4 +48,11 @@
                                          (require 'calfw)
                                          (require 'calfw-org)
                                          ))
+                          (:name emacs-w3m
+                                 :after(progn
+                                         (setq browse-url-browser-function 'w3m-browse-url)
+                                         (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+                                         ;; optional keyboard short-cut
+                                         (global-set-key "\C-xm" 'browse-url-at-point)
+                                         ))
                           )))
