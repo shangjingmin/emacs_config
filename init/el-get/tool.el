2cccd768df831cbc952a4b8404b239cc3291746c
sjm <shangjingmin@gmail.com>
2013-07-30 17:31:29 +0800

    tool: remove sdcv, just use sdcv-mode

--- init/el-get/tool.el
+++ init/el-get/tool.el
@@ -1,12 +1,13 @@
 (setq el-get-sources
  (append el-get-sources '((:name sdcv-mode
-                                 :description "emacs dictionary, need stardict and shell command sdcv"
+                                 :description "emacs dictionary, need stardict and shell command sdcv, source: http://www.cnblogs.com/bamanzi/archive/2011/06/26/emacs-stardict.html"
                                  :type git
                                  :url "https://github.com/gucong/emacs-sdcv"
                                  :branch "master"
                                  :load-path(".")
                                  :after(progn
                                          (require 'sdcv-mode)
+                                         (global-set-key (kbd "C-c d") 'sdcv-search)
                                          ))
                           (:name showtip
                                  :description "show tooltip near the cursor"
@@ -14,19 +15,6 @@
                                  :url "https://github.com/emacsmirror/showtip"
                                  :branch "master"
                                  :load-path("."))
-                          (:name sdcv
-                                 :description "emacs dictionary, can show tooltips"
-                                 :type git
-                                 :url "https://github.com/emacsmirror/sdcv"
-                                 :branch "master"
-                                 :load-path(".")
-                                 :after(progn
-                                         (require 'sdcv)
-                                         (setq sdcv-dictionary-simple-list
-                                               '("懒虫简明英汉词典" "懒虫简明汉英词典"))
-                                         (setq sdcv-dictionary-complete-list
-                                               '("牛津英汉双解美化版" "懒虫简明英汉词典" "懒虫简明汉英词典"))
-                                         ))
                           (:name weibo.emacs
                                  :description "emacs weibo"
                                  :type git
